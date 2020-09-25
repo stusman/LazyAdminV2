fb72d05a263171efd8a3471da6bf818814189bdd
KSerjyk <serg14581@gmail.com>
2020-09-25 23:01:41 +0300

    Check my window(not finished) To change to your window go to App.xaml and change following "StartupUri="LazyAdminWindow.xaml" to "StartupUri="MainWindow.xaml"

---
+++ LazyAdminWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LazyAdmin
+{
+    /// <summary>
+    /// Логика взаимодействия для LazyAdminWindow.xaml
+    /// </summary>
+    public partial class LazyAdminWindow : Window
+    {
+        public LazyAdminWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (textBox.Text == "ENTER LOGIN HERE")
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+                textBox.Text = "ENTER LOGIN HERE";
+        }
+    }
+}
