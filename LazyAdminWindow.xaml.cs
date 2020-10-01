4787e75539a183a5c64fc75b20b6aa1b248a4626
serg14581 <serg14581@gmail.com>
2020-10-01 18:56:03 +0300

    ShutdownByIP is made Testing power on

--- LazyAdminWindow.xaml.cs
+++ LazyAdminWindow.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,11 +26,65 @@ namespace LazyAdmin
     public partial class LazyAdminWindow : Window
     {
         string login, password;
+        string macAddress = "00D861D2ABD1";
+        string[] ips
+        {
+            get { return this.ips; }
+            set
+            {
+                ips = value;
+                if (ips == null)
+                {
+                    PowerOffBtn.IsEnabled = false;
+                }
+            }
+        }
+
+        private void TurnOfPCByIp(string[] ip)
+        {
+            foreach (string s in ip)
+            {
+                ProcessStartInfo shutdown = new ProcessStartInfo("shutdown", @"-m \\" + s + "-s -f -t 0");
+                shutdown.CreateNoWindow = true;
+                shutdown.UseShellExecute = false;
+                Process.Start(shutdown);
+            }
+        }
         public LazyAdminWindow()
         {
             InitializeComponent();
+            WakeFunction(macAddress);
         }
+        private void WakeFunction(string MAC_ADDRESS)
+        {
+            WOLClass client = new WOLClass();
+            client.Connect(new
+               IPAddress(0xffffffff),  //255.255.255.255  i.e broadcast
+               0x9); // port=12287 let's use this one 
+            client.SetClientToBrodcastMode();
+            //set sending bites
+            int counter = 0;
+            //buffer to be send
+            byte[] bytes = new byte[1024];   // more than enough :-)
+                                             //first 6 bytes should be 0xFF
+            for (int y = 0; y < 6; y++)
+                bytes[counter++] = 0xFF;
+            //now repeate MAC 16 times
+            for (int y = 0; y < 16; y++)
+            {
+                int i = 0;
+                for (int z = 0; z < 6; z++)
+                {
+                    bytes[counter++] =
+                        byte.Parse(MAC_ADDRESS.Substring(i, 2),
+                        NumberStyles.HexNumber);
+                    i += 2;
+                }
+            }
 
+            //now send wake up packet
+            int reterned_value = client.Send(bytes, 1024);
+        }
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox textBox = sender as TextBox;
@@ -83,6 +142,18 @@ namespace LazyAdmin
                     instalFileLbl.Content = openFileDialog.FileName;
                 }
             }
+        }
+    }
+    public class WOLClass : UdpClient
+    {
+        public WOLClass() : base()
+        { }
+        //this is needed to send broadcast packet
+        public void SetClientToBrodcastMode()
+        {
+            if (this.Active)
+                this.Client.SetSocketOption(SocketOptionLevel.Socket,
+                                          SocketOptionName.Broadcast, false);
         }
     }
 }
