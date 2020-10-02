c1605ba4958fbf484f4620ef1233b6dcb739240a
serg14581 <serg14581@gmail.com>
2020-10-02 15:30:32 +0300

    WOL DONE

--- LazyAdminWindow.xaml.cs
+++ LazyAdminWindow.xaml.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -26,7 +28,7 @@ namespace LazyAdmin
     public partial class LazyAdminWindow : Window
     {
         string login, password;
-        string macAddress = "00D861D2ABD1";
+        string macAddress = "38D547B5206A";
         string[] ips
         {
             get { return this.ips; }
@@ -53,38 +55,9 @@ namespace LazyAdmin
         public LazyAdminWindow()
         {
             InitializeComponent();
-            WakeFunction(macAddress);
+            WOL.WakeOnLan(macAddress);
         }
-        private void WakeFunction(string MAC_ADDRESS)
-        {
-            WOLClass client = new WOLClass();
-            client.Connect(new
-               IPAddress(0xffffffff),  //255.255.255.255  i.e broadcast
-               0x9); // port=12287 let's use this one 
-            client.SetClientToBrodcastMode();
-            //set sending bites
-            int counter = 0;
-            //buffer to be send
-            byte[] bytes = new byte[1024];   // more than enough :-)
-                                             //first 6 bytes should be 0xFF
-            for (int y = 0; y < 6; y++)
-                bytes[counter++] = 0xFF;
-            //now repeate MAC 16 times
-            for (int y = 0; y < 16; y++)
-            {
-                int i = 0;
-                for (int z = 0; z < 6; z++)
-                {
-                    bytes[counter++] =
-                        byte.Parse(MAC_ADDRESS.Substring(i, 2),
-                        NumberStyles.HexNumber);
-                    i += 2;
-                }
-            }
 
-            //now send wake up packet
-            int reterned_value = client.Send(bytes, 1024);
-        }
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
             TextBox textBox = sender as TextBox;
@@ -123,15 +96,15 @@ namespace LazyAdmin
         {
             Button btn = sender as Button;
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            if("Select txt file" == btn.Content.ToString())
+            if ("Select txt file" == btn.Content.ToString())
             {
                 openFileDialog.Filter = "Text files (*.txt)|*.txt";
             }
-            else if("Select exe file" == btn.Content.ToString())
+            else if ("Select exe file" == btn.Content.ToString())
             {
                 openFileDialog.Filter = "Executable files (*.exe)|*.exe";
             }
-            if(openFileDialog.ShowDialog() == true)
+            if (openFileDialog.ShowDialog() == true)
             {
                 if ("Select txt file" == btn.Content.ToString())
                 {
@@ -144,16 +117,75 @@ namespace LazyAdmin
             }
         }
     }
-    public class WOLClass : UdpClient
+    public static class WOL
     {
-        public WOLClass() : base()
-        { }
-        //this is needed to send broadcast packet
-        public void SetClientToBrodcastMode()
+
+        public static async Task WakeOnLan(string macAddress)
         {
-            if (this.Active)
-                this.Client.SetSocketOption(SocketOptionLevel.Socket,
-                                          SocketOptionName.Broadcast, false);
+            byte[] magicPacket = BuildMagicPacket(macAddress);
+            foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces().Where((n) =>
+                n.NetworkInterfaceType != NetworkInterfaceType.Loopback && n.OperationalStatus == OperationalStatus.Up))
+            {
+                IPInterfaceProperties iPInterfaceProperties = networkInterface.GetIPProperties();
+                foreach (MulticastIPAddressInformation multicastIPAddressInformation in iPInterfaceProperties.MulticastAddresses)
+                {
+                    IPAddress multicastIpAddress = multicastIPAddressInformation.Address;
+                    if (multicastIpAddress.ToString().StartsWith("ff02::1%", StringComparison.OrdinalIgnoreCase)) // Ipv6: All hosts on LAN (with zone index)
+                    {
+                        UnicastIPAddressInformation unicastIPAddressInformation = iPInterfaceProperties.UnicastAddresses.Where((u) =>
+                            u.Address.AddressFamily == AddressFamily.InterNetworkV6 && !u.Address.IsIPv6LinkLocal).FirstOrDefault();
+                        if (unicastIPAddressInformation != null)
+                        {
+                            await SendWakeOnLan(unicastIPAddressInformation.Address, multicastIpAddress, magicPacket);
+                            break;
+                        }
+                    }
+                    else if (multicastIpAddress.ToString().Equals("224.0.0.1")) // Ipv4: All hosts on LAN
+                    {
+                        UnicastIPAddressInformation unicastIPAddressInformation = iPInterfaceProperties.UnicastAddresses.Where((u) =>
+                            u.Address.AddressFamily == AddressFamily.InterNetwork && !iPInterfaceProperties.GetIPv4Properties().IsAutomaticPrivateAddressingActive).FirstOrDefault();
+                        if (unicastIPAddressInformation != null)
+                        {
+                            await SendWakeOnLan(unicastIPAddressInformation.Address, multicastIpAddress, magicPacket);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        static byte[] BuildMagicPacket(string macAddress) // MacAddress in any standard HEX format
+        {
+            macAddress = Regex.Replace(macAddress, "[: -]", "");
+            byte[] macBytes = new byte[6];
+            for (int i = 0; i < 6; i++)
+            {
+                macBytes[i] = Convert.ToByte(macAddress.Substring(i * 2, 2), 16);
+            }
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (BinaryWriter bw = new BinaryWriter(ms))
+                {
+                    for (int i = 0; i < 6; i++)  //First 6 times 0xff
+                    {
+                        bw.Write((byte)0xff);
+                    }
+                    for (int i = 0; i < 16; i++) // then 16 times MacAddress
+                    {
+                        bw.Write(macBytes);
+                    }
+                }
+                return ms.ToArray(); // 102 bytes magic packet
+            }
+        }
+
+        static async Task SendWakeOnLan(IPAddress localIpAddress, IPAddress multicastIpAddress, byte[] magicPacket)
+        {
+            using (UdpClient client = new UdpClient(new IPEndPoint(localIpAddress, 0)))
+            {
+                await client.SendAsync(magicPacket, magicPacket.Length, multicastIpAddress.ToString(), 9);
+            }
         }
     }
 }
